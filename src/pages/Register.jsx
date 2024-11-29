5b1dd2c82fbcbb1492c6b467e90674d0be89d129
Dong Ho, Shin <31787715+TLparche@users.noreply.github.com>
2024-11-30 06:36:33 +0900

    edit style

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -177,13 +177,13 @@ function Register() {
 
     return (
         <div className={"container"}>
-            <div className={"register-banner-container"}>
-                <div className={"register-banner"}>
+            <div className={"banner-container"}>
+                <div className={"banner"}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="18" viewBox="0 0 10 18" fill="none">
                         <path d="M9 1L1 9L9 17" stroke="#ACCD5E" strokeWidth="2" strokeLinecap="round"
                               strokeLinejoin="round"/>
                     </svg>
-                    <p className={"register-text"}>회원가입</p>
+                    <p className={"banner-text"}>회원가입</p>
                 </div>
             </div>
             <div className={"banner-underline"}/>
