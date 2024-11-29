9f28faf26c312ef6144400970f6026eee147da32
Dong Ho, Shin <31787715+TLparche@users.noreply.github.com>
2024-11-30 06:35:11 +0900

    add link

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import Login from '../pages/Login'
 import Register from '../pages/Register'
+import Shop from "../pages/Shop"
 
 export default function Router() {
     return (
@@ -12,10 +13,14 @@ export default function Router() {
             <Link to={"/register"}>
                 register page
             </Link>
+            <Link to={"/shop"}>
+                shop
+            </Link>
 
             <Routes>
                 <Route exact path='/login' element={<Login/>}/>
                 <Route exact path={"/register"} element={<Register/>}/>
+                <Route exact path={"/shop"} element={<Shop/>}/>
             </Routes>
         </BrowserRouter>
     );
